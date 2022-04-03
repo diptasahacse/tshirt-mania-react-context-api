b953e885cce67463cde6490b48344f0abc65ea3d
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-04 01:38:58 +0600

    conditional rendering with ternary operator

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,29 +5,35 @@ const Cart = ({cart,removeFromCart}) => {
 
     // Conditional Rendering
     // Option 1 > Element Variable
-    let message;
-    if(cart.length === 0){
-        message = <p>Please add at least one item</p>;
+    // let message;
+    // if(cart.length === 0){
+    //     message = <p>Please add at least one item</p>;
 
-    }
-    else if(cart.length === 1){
-        message = <p>Please add more item</p>;
+    // }
+    // else if(cart.length === 1){
+    //     message = <p>Please add more item</p>;
 
-    }
-    else{
-        message = <p>Thanks for adding item</p>;
+    // }
+    // else{
+    //     message = <p>Thanks for adding item</p>;
 
-    }
+    // }
+    
 
     return (
         <div>
             <h4>Cart Summery</h4>
-            {
+            {/* {
                 message
-            }
+            } */}
             <div>
                 {
                     cart.map((item,index) => <p key={index}>{item.name} <button onClick={()=>removeFromCart(item)}>X</button></p> )
+                }
+            </div>
+            <div>
+                {
+                    cart.length !== 4 ? <p>Keep Adding</p> : <button>Remove All</button>
                 }
             </div>
         </div>
