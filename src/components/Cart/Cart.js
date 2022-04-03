17d9f77c5cf40b082ad96cccdf011405895363c1
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-04 01:28:01 +0600

    conditional rendering with element variable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 const Cart = ({cart,removeFromCart}) => {
+    
+
+    // Conditional Rendering
+    // Option 1 > Element Variable
+    let message;
+    if(cart.length === 0){
+        message = <p>Please add at least one item</p>;
+
+    }
+    else if(cart.length === 1){
+        message = <p>Please add more item</p>;
+
+    }
+    else{
+        message = <p>Thanks for adding item</p>;
+
+    }
 
     return (
         <div>
             <h4>Cart Summery</h4>
+            {
+                message
+            }
             <div>
                 {
                     cart.map((item,index) => <p key={index}>{item.name} <button onClick={()=>removeFromCart(item)}>X</button></p> )
