22b423cfddb915dea5c76589d277194235739403
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-03 23:15:25 +0600

    install react router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import OrderReview from './components/OrderReview/OrderReview';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/review' element={<OrderReview></OrderReview>}></Route>
+      </Routes>
     </div>
   );
 }
